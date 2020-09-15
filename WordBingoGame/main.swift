77165907c84777c5b58a0d123355a1861949f768
近藤元気 <genki7onepiece@gmail.com>
2020-09-15 12:01:49 +0900

    Initial Commit

---
+++ WordBingoGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WordBingoGame
+//
+//  Created by 近藤元気 on 2020/09/15.
+//  Copyright © 2020 genkikondo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
